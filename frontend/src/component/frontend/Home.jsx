e2cdeed95ca1df56ae654bf5db1cd926c86d037b
dhruvlxkr <dhruvlxkr123@gmail.com>
2026-08-13 23:52:13 +0530

    chnages

--- frontend/src/component/frontend/Home.jsx
+++ frontend/src/component/frontend/Home.jsx
@@ -1,9 +1,44 @@
-import React from 'react'
+import React from "react";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
 
 const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <>
+      <div className="container py-3">
+        <header>
+          <Navbar expand="lg">
+            <Navbar.Brand href="#home" className="logo">
+              <span>Jai Sawariya</span> Construction
+            </Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+              <Nav className="ms-auto">
+                <Nav.Link href="#home" className="nav-link">
+                  Home
+                </Nav.Link>
+                <Nav.Link href="#link" className="nav-link">
+                  About Us
+                </Nav.Link>
+                <Nav.Link href="#link" className="nav-link">
+                  Services
+                </Nav.Link>
+                <Nav.Link href="#link" className="nav-link">
+                  Projects
+                </Nav.Link>
+                <Nav.Link href="#link" className="nav-link">
+                  Blogs
+                </Nav.Link>
+                <Nav.Link href="#link" className="nav-link">
+                  Contact Us
+                </Nav.Link>
+              </Nav>
+            </Navbar.Collapse>
+          </Navbar>
+        </header>
+      </div>
+    </>
+  );
+};
 
-export default Home
+export default Home;
