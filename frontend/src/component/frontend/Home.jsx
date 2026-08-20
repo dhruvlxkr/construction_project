105dee1dd7d3632131c21796577892e286093e86
dhruvlxkr <dhruvlxkr123@gmail.com>
2026-08-21 00:01:34 +0530

    changes

--- frontend/src/component/frontend/Home.jsx
+++ frontend/src/component/frontend/Home.jsx
@@ -54,30 +54,41 @@ const Home = () => {
         <section className="section-3 p-5 bg-light">
           <div className="container-fluid ">
             <div className="section-header text-center">
-                <span>Our Services</span>
-                <h2>Our construction services</h2>
-                <p>We offer a diverse array of construction services, spanning residential, commercial, and industrial projects.
-                </p>
+              <span>Our Services</span>
+              <h2>Our construction services</h2>
+              <p>
+                We offer a diverse array of construction services, spanning
+                residential, commercial, and industrial projects.
+              </p>
             </div>
           </div>
           <div className="row">
-           <div className="col-md-3 col-lg-3">
-            <div className="items">
-             <div className="service-image">
-               <img src="" alt="" />
-             </div>
-             <div className="service-body">
-                <div className="service-title">
-                 <h3>Specialty Construction</h3>
+            <div className="col-md-3 col-lg-3">
+              <div className="items">
+                <div className="service-image">
+                  <img
+                    src="../../images/construction1.jpg"
+                    alt=""
+                    className="w-100"
+                  />
                 </div>
-                <div className="service-content">
-                   <p>
-                    Speacially 
-                   </p>
+                <div className="service-body">
+                  <div className="service-title">
+                    <h3>Specialty Construction</h3>
+                  </div>
+                  <div className="service-content">
+                    <p>
+                      Speacially construction is a niche sector within the
+                      construction industry that focuses on projects requiring
+                      specialized skills, materials and techniques
+                    </p>
+                  </div>
+                  <a href="" className="btn btn-primary">
+                    Read More
+                  </a>
                 </div>
-             </div>
+              </div>
             </div>
-           </div>
           </div>
         </section>
       </main>
